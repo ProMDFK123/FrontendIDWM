b7a32c1bee46d61d8adb115c069ebe3436f09ea0
Gabriel Lopez <89820038+ProMDFK123@users.noreply.github.com>
2025-06-10 22:18:00 -0400

    feat: user authentication context added

---
+++ src/interfaces/User.tsx
@@ -0,0 +1,16 @@
+export interface User {
+    firstName?:    string;
+    lastName?:     string;
+    email:        string;
+    thelephone?:   string;
+    street?:       string;
+    number?:       string;
+    commune?:      string;
+    region?:       string;
+    postalCode?:   string;
+    registeredAt?: Date;
+    lastAccess?:   Date;
+    isActive?:     boolean;
+    token:        string;
+    role:         string;
+}
